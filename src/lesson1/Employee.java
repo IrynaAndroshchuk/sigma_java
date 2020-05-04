f8f86c2584b58a9c247d2922c8be48b7cdc5438c
Iryna Androshchuk <airenka95@gmail.com>
2020-05-04 20:04:33 +0300

    added Employee and Organization classes

---
+++ src/lesson1/Employee.java
@@ -0,0 +1,9 @@
+package lesson1;
+
+public class Employee {
+    String name;
+    String surname;
+    boolean isMarried;
+    //    experience in month
+    int experience;
+}
---
+++ src/lesson1/Organization.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+public class Organization {
+    String name;
+    String phoneNumber;
+    String address;
+    String region;
+    String country;
+    String city;
+    boolean isActive;
+    String direction;
+    int numberOfOffices;
+    Employee employee;
+}
