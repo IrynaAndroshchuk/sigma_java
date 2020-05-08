2168c341596121b75a3dc7354074c4f705b1c4cc
Iryna Androshchuk <airenka95@gmail.com>
2020-05-08 15:10:38 +0300

    controller created

---
+++ src/main/java/com/sigmaproject/domain/SigmaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sigmaproject.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigmaProjectApplication.class, args);
+    }
+
+}
