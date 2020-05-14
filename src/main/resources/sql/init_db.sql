3ea46c9d1aa00f9706b72271871c7515bd3c342d
Iryna Androshchuk <airenka95@gmail.com>
2020-05-14 22:10:34 +0300

    employees and organization tables fixed

--- src/main/resources/sql/init_db.sql
+++ src/main/resources/sql/init_db.sql
@@ -1,13 +1,26 @@
-CREATE TABLE employees (
-    id SERIAL NOT NULL CONSTRAINT employee_key PRIMARY KEY,
-    name VARCHAR(255),
-    surname VARCHAR(255),
-    organization_id INT NOT NULL,
-     CONSTRAINT fk_organizations_employees_id FOREIGN KEY (organization_id) REFERENCES organizations (id)
+CREATE TABLE organizations
+(
+    id              SERIAL       NOT NULL
+        CONSTRAINT organization_key PRIMARY KEY,
+    name            VARCHAR(255) NOT NULL,
+    phoneNumber     VARCHAR(255),
+    address         VARCHAR(255),
+    region          VARCHAR(255),
+    country         VARCHAR(255) NOT NULL,
+    city            VARCHAR(255) NOT NULL,
+    isActive        BOOLEAN      NOT NULL,
+    direction       VARCHAR(255),
+    numberOfOffices INT
 );
 
-CREATE TABLE organizations (
-id SERIAL NOT NULL CONSTRAINT organization_key PRIMARY KEY,
-name VARCHAR(255),
-amount_of_employees INT
+CREATE TABLE employees
+(
+    id              SERIAL NOT NULL
+        CONSTRAINT employee_key PRIMARY KEY,
+    name            VARCHAR(255),
+    surname         VARCHAR(255),
+    is_married      BOOLEAN,
+    experience      INT,
+    organization_id INT    NOT NULL,
+    CONSTRAINT fk_organizations_employees_id FOREIGN KEY (organization_id) REFERENCES organizations (id)
 );
